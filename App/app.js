be98980ff4fe3e37dfe6c49d0a53d5d7f7c89cc6
Bob de Vries <b.d.vries@storm-eindhoven.com>
2018-12-23 17:20:37 +0100

    Created first set of tests, and small mock-implementation to verify

--- App/app.js
+++ App/app.js
@@ -3,10 +3,26 @@
  */
 
 //Load Express and put it into our App
-var express = require("express");
+var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 //Open ears to port 3000, giving a yell if succesfull
 app.listen(3000, function() {
-	console.log("Beep Boop Banking on port 3000");
+	console.log('Beep Boop Banking on port 3000');
+});
+
+app.post('/account',function(req,res) {
+	var name = req.body.name;
+	if(typeof req.body.name == 'undefined' || req.body.name==null || req.body.name== ''){
+		res.status(400).send('You forgot to send the name.');
+	}
+	else {
+		res.status(200).send({
+			'name':name,
+			'balance':0,
+			'id':12
+		});
+	}
 });
--- App/tests/postaccount.test.js
+++ App/tests/postaccount.test.js
@@ -13,19 +13,22 @@ describe('POST account', function() {
 	describe('should give an error', function() {
 		it('if input is omitted', function(done) {
 			request.send({}).end(function(err,res) {
-				expect(err).to.not.be.empty;
+				expect(res).to.not.be.empty;
+				expect(res.status).to.not.be.equal(200);
 				done();
 			});
 		});
 		it('if input is null', function(done) {
 			request.send({'name': null}).end(function(err,res) {
-				expect(err).to.not.be.empty;
+				expect(res).to.not.be.empty;
+				expect(res.status).to.not.be.equal(200);
 				done();
 			});
 		});	
 		it('if input is an empty string', function(done) {
 			request.send({'name': ''}).end(function(err,res) {
-				expect(err).to.not.be.empty;
+				expect(res).to.not.be.empty;
+				expect(res.status).to.not.be.equal(200);
 				done();
 			});			
 		});			
@@ -38,10 +41,68 @@ describe('POST account', function() {
 		it('a balance of 0', function(done) {
 			request.send({'name':'Timo Test'}).end(function(err,res) {
 				expect(err).to.be.null;
+				expect(res.status).to.be.equal(200);
 				expect(res).to.not.be.empty;
 				expect(res.body).to.include({'balance':0});
 				done();
 			});
 		});
+		it('a name equal to the input', function(done) {
+			var checkName = 'Pieter Probeer'
+			request.send({'name':checkName}).end(function(err,res) {
+				expect(err).to.be.null;
+				expect(res.status).to.be.equal(200);				
+				expect(res).to.not.be.empty;
+				expect(res.body).to.include({'name':checkName});
+				done();
+			});
+		});
+		it('a complete set of inputs', function(done) {
+			var checkName = ' Karel Keur'
+			request.send({'name':checkName}).end(function(err,res) {
+				expect(err).to.be.null;
+				expect(res.status).to.be.equal(200);				
+				expect(res).to.not.be.empty;
+				expect(res.body).to.include({'name':checkName});
+				expect(res.body).to.include({'balance':0});
+				expect(res.body).to.ownPropertyDescriptor('id');
+				done();
+			});
+		});		
 	});
+	describe('should create unique ids', function() {
+		it('for 2 same names', function(done) {
+			var sameName='Teun Toets';
+			chai.request(appUrl).post('/account').send({'name':sameName}).end(function(err,res){
+				expect(err).to.be.null;
+				expect(res.status).to.be.equal(200);				
+				expect(res).to.not.be.empty;
+				//This is a bit weird but we're abusing javascript callbacks for timing
+				var prevId=res.body.id;
+				chai.request(appUrl).post('/account').send({'name':sameName}).end(function(err,res){
+					expect(err).to.be.null;
+					expect(res.status).to.be.equal(200);				
+					expect(res).to.not.be.empty;
+					expect(res.body.id).to.not.be.equal(prevId);
+					done();
+				});
+			});
+		});
+		it('for 2 different names', function(done) {
+			chai.request(appUrl).post('/account').send({'name':'Chad Check'}).end(function(err,res){
+				expect(err).to.be.null;
+				expect(res.status).to.be.equal(200);				
+				expect(res).to.not.be.empty;
+				//This is a bit weird but we're abusing javascript callbacks for timing
+				var prevId=res.body.id;
+				chai.request(appUrl).post('/account').send({'name':'Mieke Meet'}).end(function(err,res){
+					expect(err).to.be.null;
+					expect(res.status).to.be.equal(200);					
+					expect(res).to.not.be.empty;
+					expect(res.body.id).to.not.be.equal(prevId);
+					done();
+				});
+			});
+		});		
+	})
 });
