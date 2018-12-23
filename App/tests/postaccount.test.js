8a1111fd2f173e7e1d7d01df773cca9ea8f0e1ec
Bob de Vries <b.d.vries@storm-eindhoven.com>
2018-12-23 15:45:53 +0100

    Added missing callback that caused silent okays for tests

--- App/tests/postaccount.test.js
+++ App/tests/postaccount.test.js
@@ -8,23 +8,40 @@ var appUrl = 'http://localhost:3000'
 describe('POST account', function() {
 	var request;
 	beforeEach(function() {
-		request = chai.request(appUrl).put('/account');
+		request = chai.request(appUrl).post('/account');
 	});
 	describe('should give an error', function() {
-		it('if input is omitted', function() {
+		it('if input is omitted', function(done) {
 			request.send({}).end(function(err,res) {
-				expect(err);
+				expect(err).to.not.be.empty;
+				done();
 			});
 		});
-		it('if input is null', function() {
-			request.send({"name": null}).end(function(err,res) {
-				expect(err);
+		it('if input is null', function(done) {
+			request.send({'name': null}).end(function(err,res) {
+				expect(err).to.not.be.empty;
+				done();
 			});
 		});	
-		it('if input is an empty string', function() {
-			request.send({"name": ""}).end(function(err,res) {
-				expect(err);
+		it('if input is an empty string', function(done) {
+			request.send({'name': ''}).end(function(err,res) {
+				expect(err).to.not.be.empty;
+				done();
 			});			
 		});			
 	});	
+	describe('should create an account with', function() {
+		var request;
+		beforeEach(function() {
+			request = chai.request(appUrl).post('/account');
+		});
+		it('a balance of 0', function(done) {
+			request.send({'name':'Timo Test'}).end(function(err,res) {
+				expect(err).to.be.null;
+				expect(res).to.not.be.empty;
+				expect(res.body).to.include({'balance':0});
+				done();
+			});
+		});
+	});
 });
