ad9797efdf7d80b49044c837acd5de8c299ba657
Bob de Vries <b.d.vries@storm-eindhoven.com>
2018-12-23 17:31:23 +0100

    Added unique ids as a feature

--- App/app.js
+++ App/app.js
@@ -8,6 +8,18 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+var mapOfIds = [];
+
+function getNewId(){
+	var newId = 10000 +Math.round(Math.random()*89999);
+	while(newId in mapOfIds) {
+		newId = 10000 +Math.round(Math.random()*89999)
+	}
+	//Mark that we are using this
+	mapOfIds[newId]=1;
+	return newId;
+};
+
 //Open ears to port 3000, giving a yell if succesfull
 app.listen(3000, function() {
 	console.log('Beep Boop Banking on port 3000');
@@ -22,7 +34,7 @@ app.post('/account',function(req,res) {
 		res.status(200).send({
 			'name':name,
 			'balance':0,
-			'id':12
+			'id':getNewId()
 		});
 	}
 });
